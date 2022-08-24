3d58d348d6722521460470f682e4e729649b2159
Ian Greenfield <ian.greenfield@heydayskincare.com>
2022-08-24 08:49:21 -0500

    image test

--- components/ThreeColumn.js
+++ components/ThreeColumn.js
@@ -11,12 +11,11 @@ export default function ThreeColumn(props) {
                         <Text textAlign="center" fontSize='18px'>Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor.</Text>
                     </Stack>
                     <SimpleGrid columns={[1,3]} spacing={10} mt='64px'>
-                        <Box>
+                        <Box pos="relative" w="100%" h="200px">
                             <Image 
                                 src="https://cdn.shopify.com/s/files/1/1704/8471/files/Naturopathica_2.png?v=1650278436"
-                                layout="responsive"
-                                height={275}
-                                width={300}
+                                layout="fill"
+                                objectFit="contain"
                             />
                             <Heading as='h3' fontSize='24px' fontWeight={500} textAlign="center" mt='16px' px='24px'>Lorem ipsum dolor mont blanc</Heading>
                         </Box>
