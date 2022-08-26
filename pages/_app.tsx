37c315ed7008bc95219b0fb91887142a5cd4af76
Ian Greenfield <ian.greenfield@heydayskincare.com>
2022-08-26 13:11:48 -0500

    facebook pixel test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import { builder } from '@builder.io/react'
 import builderConfig from '../config/builder'
 import '../assets/index.css'
 import { ChakraProvider } from '@chakra-ui/react'
-import '@components/FacebookPixel'
 //import Fonts from '@assets/styles/Fonts'
 import Head from 'next/head'
 import theme from '../assets/styles/theme'
@@ -19,7 +18,6 @@ import "../components/ThreeColumn/ThreeColumn.builder"
 import "../components/VideoHero/VideoHero.builder"
 import "../components/FormHero/FormHero.builder"
 import Script from 'next/script'
-import FacebookPixel from '@components/FacebookPixel'
 //import "../components/AnnouncementBar/AnnouncementBar.builder"
 
 
@@ -28,13 +26,28 @@ builder.init(builderConfig.apiKey)
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 
+  const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID
+
   return (
     <ChakraProvider theme={theme}>
       <Head>
         <link rel="icon" href="/static/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Script async src="https://www.googletagmanager.com/gtag/js?id=UA-42488621-18"></Script>
-        <FacebookPixel />
+        <Script id="facebook-pixel">
+      {`
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version=2.0;
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,script,
+        https://connect.facebook.net/en_US/fbevents.js);
+        fbq(init, ${FB_PIXEL_ID});
+        fbq(track, PageView);
+      `}
+    </Script>
       </Head>
       <Component {...pageProps} />
       <script type="text/javascript" async src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=UFdULB"></script>
