ab8b7fc4f473596e87396b62ae09267bc70d56d3
Ian Greenfield <ian.greenfield@heydayskincare.com>
2022-08-26 13:31:15 -0500

    fb tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,31 +26,29 @@ builder.init(builderConfig.apiKey)
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 
-  const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID
-
   return (
     <ChakraProvider theme={theme}>
       <Head>
         <link rel="icon" href="/static/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Script async src="https://www.googletagmanager.com/gtag/js?id=UA-42488621-18"></Script>
-        <Script id="facebook-pixel">
-      {`
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version=2.0;
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,script,
-        https://connect.facebook.net/en_US/fbevents.js);
-        fbq(init, ${FB_PIXEL_ID});
-        fbq(track, PageView);
-      `}
-    </Script>
+        <script type="text/javascript" async src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=UFdULB"></script>
       </Head>
       <Component {...pageProps} />
-      <script type="text/javascript" async src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=UFdULB"></script>
+      <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1026552650761735');
+      fbq('track', 'PageView');` }}
+    />
+    <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=1026552650761735&ev=PageView&noscript=1" />` }}
+    />
     </ChakraProvider>
   )
 }
