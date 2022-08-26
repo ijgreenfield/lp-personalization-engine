89a0167c412b309541e312f5e651c4a4d0276861
Ian Greenfield <ian.greenfield@heydayskincare.com>
2022-08-26 09:48:54 -0500

    remove fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,21 +3,21 @@ import { builder } from '@builder.io/react'
 import builderConfig from '../config/builder'
 import '../assets/index.css'
 import { ChakraProvider } from '@chakra-ui/react'
-import Fonts from '@assets/styles/Fonts'
+//import Fonts from '@assets/styles/Fonts'
 import Head from 'next/head'
 import theme from '../assets/styles/theme'
 import "../components/CtaSection/CtaSection.builder"
-//import "../components/FAQ/FAQ.builder"
+import "../components/FAQ/FAQ.builder"
 import "../components/Footer/Footer.builder"
-//import "../components/Header/Header.builder"
+import "../components/Header/Header.builder"
 import "../components/HeaderTransparent/HeaderTransparent.builder"
-//import "../components/Hero/Hero.builder"
+import "../components/Hero/Hero.builder"
 import "../components/LogoCloud/LogoCloud.builder"
 import "../components/Testimonials/Testimonials.builder"
 import "../components/ThreeColumn/ThreeColumn.builder"
 import "../components/VideoHero/VideoHero.builder"
-//import "../components/FormHero/FormHero.builder"
-//import "../components/AnnouncementBar/AnnouncementBar.builder"
+import "../components/FormHero/FormHero.builder"
+import "../components/AnnouncementBar/AnnouncementBar.builder"
 
 
 builder.init(builderConfig.apiKey)
@@ -30,7 +30,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
-      <Fonts />
       <script type="text/javascript" async src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=UFdULB"></script>
     </ChakraProvider>
   )
