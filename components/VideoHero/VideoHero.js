d0885163a61693fa7f8752a5aa39490accf7e4d1
Ian Greenfield <ian.greenfield@heydayskincare.com>
2022-08-26 08:06:22 -0500

    responsive stylings

--- components/VideoHero/VideoHero.js
+++ components/VideoHero/VideoHero.js
@@ -5,9 +5,9 @@ import { MdLocationPin } from "react-icons/md";
 export default function VideoHero(props) {
     return (
         <Box>
-            <Box pos="relative" h="100%" pt={["104px", "0"]} pb={["48px", "0"]}>
-                <Flex pos={["relative","absolute"]} top="0" right="0" left="0" bottom="0" zIndex={5} m={["0","auto"]} px="20px" h="max-content" w={["100%","80%"]} justify="space-between" alignItems="center" flexDirection={["column", "row"]}>
-                    <Stack maxW={["100%","50%"]} textAlign={["center", "left"]} spacing="24px">
+            <Box pos="relative" h="100%" pt={["104px", null, null, "0"]} pb={["48px", null, null, "0"]}>
+                <Flex pos={["relative",null, null,"absolute"]} top="0" right="0" left="0" bottom="0" zIndex={5} m={["0","auto"]} px="20px" h="max-content" w={["100%","80%"]} justify="space-between" alignItems="center" flexDirection={["column", null, null, "row"]}>
+                    <Stack maxW={["100%", null, null, "50%"]} textAlign={["center", "left"]} spacing="24px">
                         <Heading color="#fff" as="h1" size="3xl" lineHeight="125%" textAlign={["left"]} fontFamily="Cadiz SemiBold" fontWeight="700" fontStyle="italic">Your Exclusive Offer</Heading>
                         <Text color="#fff" fontSize="14px" display={["none", "block"]}>Heyday® is more than a facial. It’s a journey of self-discovery – measured in lorem, championed by the community, and fueled by the desire to lorem ipsum dolor blah. BOOK NOW & GET YOUR FIRST FACIAL HALF OFF at participating locations.</Text>
                     </Stack>
@@ -32,20 +32,20 @@ export default function VideoHero(props) {
                     <Text color="#fff" fontSize="14px" display={["none", "none"]}>Heyday® is more than a facial. It’s a journey of self-discovery – measured in lorem, championed by the community, and fueled by the desire to lorem ipsum dolor blah. BOOK NOW & GET YOUR FIRST FACIAL HALF OFF at participating locations.</Text>
                 </Flex>
                 <Box bg="rgb(0,0,0,.5)" pos="absolute" top="0" left="0" zIndex={2} w="100%" h="100%"></Box>
-                <Box pos="absolute" top="0" left="0" zIndex={1} w="100%" h="100%" display={["block", null, "none"]}>
+                <Box pos="absolute" top="0" left="0" zIndex={1} w="100%" h="100%" display={["block", null, "block", "none"]}>
                     <Image 
                             src="https://cdn.shopify.com/s/files/1/1704/8471/files/Recess_HeyDay-66_1.png?v=1659042552"
                             layout="fill"
                             objectFit="cover"
                         />
                 </Box>
-                {/*<Box zIndex={1} h="100%" display={["none", null, "block"]}>
+                <Box zIndex={1} h="100%" display={["none", null, "block"]}>
                     <Box minH="80vh">
                         <video muted autoPlay loop width="100%" height="100%">
                             <source src="https://cdn.shopify.com/videos/c/o/v/328ec4937774489088ea9fc2eef95cfc.mp4"/>
                         </video>
                     </Box>
-                </Box>*/}
+                </Box>
             </Box>
         </Box>
     )
