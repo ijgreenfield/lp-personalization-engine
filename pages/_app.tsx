9c031a8f4ae2ac27af14779d79cac1154211ec45
Ian Greenfield <ian.greenfield@heydayskincare.com>
2022-08-26 12:32:38 -0500

    facebook pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,7 @@ import "../components/Testimonials/Testimonials.builder"
 import "../components/ThreeColumn/ThreeColumn.builder"
 import "../components/VideoHero/VideoHero.builder"
 import "../components/FormHero/FormHero.builder"
+import Script from 'next/script'
 //import "../components/AnnouncementBar/AnnouncementBar.builder"
 
 
@@ -28,6 +29,26 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="icon" href="/static/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-42488621-18"></script>
+        <Script>
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1026552650761735'); 
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+        <img height="1" width="1" 
+        src="https://www.facebook.com/tr?id=1026552650761735&ev=PageView
+        &noscript=1"/>
+        </noscript>
       </Head>
       <Component {...pageProps} />
       <script type="text/javascript" async src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=UFdULB"></script>
