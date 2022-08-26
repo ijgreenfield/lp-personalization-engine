8394b7aefdc0cc0c60c7f9288bad4facea004601
Ian Greenfield <ian.greenfield@heydayskincare.com>
2022-08-26 13:45:09 -0500

    image priority

--- components/VideoHero/VideoHero.js
+++ components/VideoHero/VideoHero.js
@@ -30,6 +30,7 @@ export default function VideoHero(props) {
                             src="https://cdn.shopify.com/s/files/1/1704/8471/files/Recess_HeyDay-66_1.png?v=1659042552"
                             layout="fill"
                             objectFit="cover"
+                            priority
                         />
                 </Box>
                 <Box zIndex={1} h="100%" display={["none", null, "block"]}>
