8aa70f76c3a6f113856320dd601a23abaf7565af
Ian Greenfield <ian.greenfield@heydayskincare.com>
2022-08-29 09:46:25 -0500

    href

--- components/ThreeColumnCards/ThreeColumnCards.js
+++ components/ThreeColumnCards/ThreeColumnCards.js
@@ -1,25 +1,27 @@
-import { Box, Container, Flex, Stack, Heading, Text } from "@chakra-ui/react"
+import { Box, Container, Flex, Stack, Heading, Text, Link } from "@chakra-ui/react"
 import Image from "next/image"
-import Link from "next/link"
 
 const offers = [
     {
         image: 'https://cdn.shopify.com/s/files/1/1704/8471/files/Group_Look_03_073_1_1.png?v=1661783186',
         headling: 'The Gift of Giving',
         description: 'Refer up to 3 friends for a free facial & receive a $25 gift card for each one! Let your friend know to use code MEMBERFRIENDCOMP when booking.',
-        cta: 'Book a Facial'
+        cta: 'Book a Facial',
+        href: '/'
     },
     {
         image: 'https://cdn.shopify.com/s/files/1/1704/8471/files/Screen_Shot_2022-08-15_at_11.05_1.png?v=1661783464',
         headling: 'Early Access to Microcurrent',
         description: 'Feel that spark? It’s not just the connection of skincare and self-care—it’s our latest enhancement, Microcurrent. We’re giving members the chance to try it for a limited time at $20 (an $80 value)!',
-        cta: 'Book a Facial'
+        cta: 'Book a Facial',
+        href: '/'
     },
     {
         image: 'https://cdn.shopify.com/s/files/1/1704/8471/files/tyraBookerr_af661089-3980-441a-9272-1f158a1e586e.png?v=1661783489',
         headling: '25% Off Skincare Essentials',
         description: 'On top of your usual 15% product discount, enjoy an extra 25% off purchases of $200 or more when you purchase products in shop or online!',
-        cta: 'Shop New Arrivals'
+        cta: 'Shop New Arrivals',
+        href: '/'
     }
 ]
 
@@ -45,7 +47,7 @@ export default function ThreeColumnCards() {
                                 <Stack textAlign="left" spacing="16px" p="20px">
                                     <Heading as="h3" fontSize="24px">{offer.headling}</Heading>
                                     <Text>{offer.description}</Text>
-                                    <Link href="/">{offer.cta}</Link>
+                                    <Link href={offer.href} textDecoration="underline" color="#105368" fontWeight={600}>{offer.cta}</Link>
                                 </Stack>
                             </Box>
                         ))}
