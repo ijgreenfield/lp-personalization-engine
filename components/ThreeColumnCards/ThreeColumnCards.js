6d2fb1edfe1ebf24dbd042d66d82ab5320d65539
Ian Greenfield <ian.greenfield@heydayskincare.com>
2022-08-29 11:32:53 -0500

    three column card links

--- components/ThreeColumnCards/ThreeColumnCards.js
+++ components/ThreeColumnCards/ThreeColumnCards.js
@@ -7,21 +7,21 @@ const offers = [
         headling: 'The Gift of Giving',
         description: 'Refer up to 3 friends for a free facial & receive a $25 gift card for each one! Let your friend know to use code MEMBERFRIENDCOMP when booking.',
         cta: 'Book a Facial',
-        href: '/'
+        href: 'https://www.heydayskincare.com/#book-now'
     },
     {
         image: 'https://cdn.shopify.com/s/files/1/1704/8471/files/Screen_Shot_2022-08-15_at_11.05_1.png?v=1661783464',
         headling: 'Early Access to Microcurrent',
         description: 'Feel that spark? It’s not just the connection of skincare and self-care—it’s our latest enhancement, Microcurrent. We’re giving members the chance to try it for a limited time at $20 (an $80 value)!',
         cta: 'Book a Facial',
-        href: '/'
+        href: 'https://www.heydayskincare.com/#book-now'
     },
     {
         image: 'https://cdn.shopify.com/s/files/1/1704/8471/files/tyraBookerr_af661089-3980-441a-9272-1f158a1e586e.png?v=1661783489',
         headling: '25% Off Skincare Essentials',
         description: 'On top of your usual 15% product discount, enjoy an extra 25% off purchases of $200 or more when you purchase products in shop or online!',
         cta: 'Shop New Arrivals',
-        href: '/'
+        href: 'https://www.heydayskincare.com/collections/new'
     }
 ]
 
