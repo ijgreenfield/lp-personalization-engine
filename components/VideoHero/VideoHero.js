aff50a61116adefe650ac6d4a193d4f56adb4456
Ian Greenfield <ian.greenfield@heydayskincare.com>
2022-08-30 08:53:30 -0500

    fix video display styling

--- components/VideoHero/VideoHero.js
+++ components/VideoHero/VideoHero.js
@@ -30,7 +30,7 @@ export default function VideoHero(props) {
                     <Text color="#fff" fontSize="14px" display={["none", "none"]}>Heyday® is more than a facial. It’s a journey of self-discovery – measured in lorem, championed by the community, and fueled by the desire to lorem ipsum dolor blah. BOOK NOW & GET YOUR FIRST FACIAL HALF OFF at participating locations.</Text>
                 </Flex>
                 <Box bg="rgb(0,0,0,.5)" pos="absolute" top="0" left="0" zIndex={2} w="100%" h="100%"></Box>
-                <Box pos="absolute" top="0" left="0" zIndex={1} w="100%" h="100%" display={["block", null, "block", "none"]}>
+                <Box pos="absolute" top="0" left="0" zIndex={1} w="100%" h="100%" display={["block", null, null, null, "none"]}>
                     <Image 
                             src="https://cdn.shopify.com/s/files/1/1704/8471/files/Recess_HeyDay-66_1.png?v=1659042552"
                             layout="fill"
@@ -38,7 +38,7 @@ export default function VideoHero(props) {
                             priority
                         />
                 </Box>
-                <Box zIndex={1} h="100%" display={["none", null, null, "block"]}>
+                <Box zIndex={1} h="100%" display={["none", null, null, null, "block"]}>
                     <Box minH="100%">
                         <video muted autoPlay loop width="100%" height="100%">
                             <source src="https://cdn.shopify.com/videos/c/o/v/328ec4937774489088ea9fc2eef95cfc.mp4"/>
