4fd9536065093d7be3d8bb68ec471bd80146dee4
Ian Greenfield <ian.greenfield@heydayskincare.com>
2022-09-20 16:26:01 -0500

    membership cards text

--- components/ThreeColumnCards/ThreeColumnCards.builder.js
+++ components/ThreeColumnCards/ThreeColumnCards.builder.js
@@ -7,4 +7,9 @@ const LazyThreeColumnCards = dynamic(async () => {
 
 Builder.registerComponent(LazyThreeColumnCards, {
     name: 'Three Column Cards',
+    inputs: [
+      { name: 'offer1', type: 'string'},
+      { name: 'offer2', type: 'string'},
+      { name: 'offer3', type: 'string'}
+    ]
 })
