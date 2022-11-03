7a8b9ab503ad7d70583201b5e7ef5da7a1b09cf2
Ian Greenfield <ian.greenfield@heydayskincare.com>
2022-11-03 09:34:06 -0500

    remove fb pixel code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       </Head>
       <Fonts />
       <Component {...pageProps} />
-      <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s)
+      {/*<script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s)
       {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
       n.callMethod.apply(n,arguments):n.queue.push(arguments)};
       if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -33,7 +33,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     />
     <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style="display:none"
       src="https://www.facebook.com/tr?id=1026552650761735&ev=PageView&noscript=1" />` }}
-    />
+      />*/}
     </>
   )
 }
