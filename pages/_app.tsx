775d1a7bda7558ff001f7bef640df9dd062903a2
Ian Greenfield <ian.greenfield@heydayskincare.com>
2022-11-07 11:50:47 -0600

    add facebook pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,30 +3,33 @@ import { builder } from '@builder.io/react'
 import builderConfig from '../config/builder'
 import '../assets/index.css'
 import Fonts from '@assets/styles/Fonts'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 
 builder.init(builderConfig.apiKey)
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 
+  const router = useRouter()
+
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('1026552650761735') // facebookPixelId
+        ReactPixel.pageView()
+
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView()
+        })
+      })
+    }, [router.events])
+
   return (
     <>
       <Fonts />
       <Component {...pageProps} />
-      {/*<script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}(window, document,'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '1026552650761735');
-      fbq('track', 'PageView');` }}
-    />
-    <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style="display:none"
-      src="https://www.facebook.com/tr?id=1026552650761735&ev=PageView&noscript=1" />` }}
-      />*/}
     </>
   )
 }
