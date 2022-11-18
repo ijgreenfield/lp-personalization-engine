cdc4731f68573323d1fb5ae3fe6d86e4bccfe52e
Ian Greenfield <ian.greenfield@heydayskincare.com>
2022-11-18 13:27:23 -0600

    retry nextjs-google-analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import '../assets/index.css'
 import Fonts from '@assets/styles/Fonts'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import Script from 'next/script'
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 
 builder.init(builderConfig.apiKey)
@@ -31,18 +31,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Script  src="https://www.googletagmanager.com/gtag/js?id=UA-42488621-18"></Script>
-      <Script
-      id='google-analytics'
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag(‘js’, new Date());
-          gtag(‘config’, ‘UA-42488621-18’);
-        `,
-        }}
-    />
+      <GoogleAnalytics trackPageViews />
       <Fonts />
       <Component {...pageProps} />
       <script async type="text/javascript" src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=UFdULB"></script>
